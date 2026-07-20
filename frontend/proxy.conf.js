6450c7c6249bc4a78867662eb3246b3ade54c9aa
Artyom Hovhannisyan <artyomhovhannisyan910@gmail.com>
2026-07-20 12:59:37 +0400

    change

--- frontend/proxy.conf.js
+++ frontend/proxy.conf.js
@@ -1,17 +1,16 @@
 module.exports = {
   '/auth': {
-    target: 'http://localhost:8080',
+    target: 'http://localhost:8090',
     secure: false,
     changeOrigin: true,
     bypass: function (req) {
-      // Browser navigation (text/html) → let Angular router handle it
       if (req.headers['accept'] && req.headers['accept'].includes('text/html')) {
         return '/index.html';
       }
     }
   },
   '/api': {
-    target: 'http://localhost:8080',
+    target: 'http://localhost:8090',
     secure: false,
     changeOrigin: true
   }
